6326f46f718932c67103fbad3b7e78432d287ce8
EmpiricalCode <daniellongschool@gmail.com>
2025-08-26 17:13:17 -0400

    feat: add run agent script for drift env

---
+++ src/run_agent.py
@@ -0,0 +1,27 @@
+
+from matplotlib import pyplot as plt
+import torch
+from sim.env import DriftSimEnv
+from sim.agent import DriftSimAgent
+
+sim_env = DriftSimEnv(track_radius=10)
+sim_agent = DriftSimAgent(env=sim_env)
+
+sim_agent.learn(1000)
+
+plt.ion()
+
+for i in range(1000):
+    obs, _ = sim_env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        action, _ = sim_agent.get_action(torch.tensor(obs, dtype=torch.float32))
+        obs, reward, _, done, _ = sim_env.step(action)
+        total_reward += reward
+
+        plt.imshow(sim_env.render(), cmap='gray')
+        plt.axis('off')
+        plt.pause(0.001)
+        plt.clf()
